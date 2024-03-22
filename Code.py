2431a3488d7d1f6347b7ec01d7452676611facaa
MiguelAlejandroB <mibermudezc@unal.edu.co>
2024-03-21 21:54:49 -0500

    Prueba de grafica de distribucion

--- Code.py
+++ Code.py
@@ -1,8 +1,10 @@
 import numpy as np #Importo las librerias que voy a usar fuera de la clase
 import pandas as pd
 from scipy.stats import norm, lognorm, gumbel_r, pearson3,  genextreme
+import matplotlib.pyplot as plt
 
 class Anla:
+
     df = pd.read_csv("est_25027400_limpia.csv")
     # Establecer la primera columna como el índice del DataFrame
     df.set_index(df.columns[0], inplace=True)
@@ -58,3 +60,42 @@ class Anla:
         n = pearson3.pdf(x, a, m, s)
         vesp.append(n)
     corp = np.corrcoef(df_minimos_por_ano['cuenca-base'], vesp)[0, 1]
+
+    #Sacar el mayor de las correlaciones y sacar en 10 años cuanto es el 7Q10
+    TR = 10
+    pe = 1 - (1 / TR)
+    def mc(a, b, c, d):
+        maximo = max(a, b, c, d)  # Encuentra el máximo entre los tres valores
+        if a == maximo:  # Comprueba si a es el máximo
+            return 1
+        elif b == maximo:  # Comprueba si b es el máximo
+            return 2
+        elif c == maximo:  # Si no es a ni b, entonces c es el máximo
+            return 3
+        else:
+            return 4
+
+
+    Bin= mc(corn,corln, corg, corp)
+    if bin == 1 :
+        S7Q10 = Fn(pe, mun, stdn)
+    elif bin ==2 :
+        S7Q10 = Flogn(pe, pln[0], pln[1])
+    elif bin == 3 :
+        S7Q10 = gum(pe, mug, beta)
+    else:
+        S7Q10 = pearson3.pdf(pe, a, m, s)
+    print(S7Q10)
+    #0.003517763085733081
+    # Crea un rango de valores en el eje x
+    x_values = np.linspace(0, 100, 1000)
+    # Evalúa la función de densidad de probabilidad en el rango de valores x
+    y_values = Fn(x_values, mun, stdn)
+    # Trazar la gráfica
+    plt.plot(x_values, y_values, label='PDF')
+    plt.xlabel('Valores')
+    plt.title('Función de densidad de probabilidad')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
