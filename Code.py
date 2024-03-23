7e2c4f1f811240f0494a48f66b993e0912889f48
MiguelAlejandroB <mibermudezc@unal.edu.co>
2024-03-22 22:23:10 -0500

    Sin gitignore

--- Code.py
+++ Code.py
@@ -97,6 +97,108 @@ class Anla:
         print(S7Q10)
 
         #Calcular
+
+
+
+    def Q95(df) -> pd.DataFrame:
+
+
+      data = estado.data
+      if estado.h_umbrales['QB'] is None:
+        u_qb = 2.33
+        u_qtq = 2
+      else:
+        u_qb = estado.h_umbrales[0]
+        u_qtq = estado.h_umbrales[1]
+      # df es un dataframe temporar que se sobreescribe eventualmente
+      df = pd.DataFrame()
+      # set columns
+      df = df.assign(Fecha=None, Min=None, Max=None, Mean=None)
+      # se crea un dataframe en donde se calculan los minimos máximos y promedio por mes y año
+      for i in range(data.index.min().year, data.index.max().year + 1):
+        for j in range(1, 13):
+          # filtro los datos por año y mes para hacer el analisis
+          data_filter = data[data.index.year == i][data[data.index.year == i].index.month == j]
+          row = [str(i) + str(-j), data_filter['cuenca-base'].min(), data_filter['cuenca-base'].max(),
+                 data_filter['cuenca-base'].mean()]
+          df.loc[len(df)] = row
+      # data view
+      df['Fecha'] = pd.to_datetime(df['Fecha'])
+      df = df.set_index('Fecha')
+      '''
+      se crea listas con n valores, siendo n el número de años en el dataset, guardando el máximo y minimo por año
+      '''
+      mins = []
+      maxs = []
+      for i in range(df.index.min().year, df.index.max().year + 1):
+        mins.append(df[df.index.year == i]['Min'].min())
+        maxs.append(df[df.index.year == i]['Max'].max())
+      mins = np.array(mins)
+      maxs = np.array(maxs)
+      mean_min = (mins.mean())
+      std_min = (np.std(mins, ddof=1))  # desviacion estandar
+      coef_variacion_min = std_min / mean_min
+      mean_max = (maxs.mean())
+      std_max = (np.std(maxs, ddof=1))
+      alpha_min = 1.0079 * (coef_variacion_min ** -1.084)
+      a_alpha_min = (-0.0607 * (coef_variacion_min ** 3)) + (0.5502 * (coef_variacion_min ** 2)) - (
+                0.4937 * coef_variacion_min) + 1.003
+      beta = (a_alpha_min / mean_min) ** (-1)
+      # umbrales QTQ y QB
+      #
+      umbral_QTQ = beta * ((-np.log(1 - (1 / u_qtq))) ** (1 / alpha_min))
+      umbral_Q10 = beta * ((-np.log(1 - (1 / 10))) ** (1 / alpha_min))
+      #
+      #
+      df = pd.DataFrame()  # crea un dataframe donde poner los minimos y maximos por mes de toda la serie
+      # set columns
+      df = df.assign(Fecha=None, Min=None, Max=None, Mean=None, Min_rev=None)
+      # calculate mim, max, mean, min_rev and mean_rev
+      for i in range(data.index.min().year, data.index.max().year + 1):
+        for j in range(1, 13):
+          # filtro los datos por año y mes para hacer el analisis
+          data_filter = data[(data.index.year == i) & (data.index.month == j)]
+          row = [str(i) + str(-j),
+                 data_filter['cuenca-base'].min(),
+                 data_filter['cuenca-base'].max(),
+                 data_filter['cuenca-base'].mean(),
+                 data_filter['cuenca-base'][data['cuenca-base'] > umbral_Q10].min()]
+          df.loc[len(df)] = row
+      df['Fecha'] = pd.to_datetime(df['Fecha'])
+      # df = df.set_index('Fecha')
+      alpha_max = (np.sqrt(6) * std_max) / np.pi
+      u_max = mean_max - (0.5772 * alpha_max)
+      yt_QB = -np.log(np.log(u_qb / (u_qb - 1)))
+      yt_Q15 = -np.log(np.log(15 / (15 - 1)))
+      # umbrales QB y QTR15
+      #
+      umbral_QB = u_max + (yt_QB * alpha_max)
+      umbral_Q15 = u_max + (yt_Q15 * alpha_max)
+      #
+      ##
+      df = pd.DataFrame()
+      df = df.assign(Fecha=None, Min=None, Max=None, Mean=None, Min_rev=None, Mean_rev=None)
+      for i in range(data.index.min().year, data.index.max().year + 1):
+        for j in range(1, 13):
+          # filtro los datos por año y mes para hacer el analisis
+          data_filter = data[(data.index.year == i) & (data.index.month == j)]
+          row = [str(i) + str(-j),
+                 data_filter['cuenca-base'].min(),
+                 data_filter['cuenca-base'].max(),
+                 data_filter['cuenca-base'].mean(),
+                 data_filter['cuenca-base'][data['cuenca-base'] > umbral_Q10].min(),
+                 data_filter['cuenca-base'][(data['cuenca-base'] > umbral_Q10) & (data['cuenca-base'] < umbral_Q15)].mean()]
+          df.loc[len(df)] = row
+      #########
+      df['Fecha'] = pd.to_datetime(df['Fecha'])
+      df = df.set_index('Fecha')
+      estado.umbrales['QTR15'] = umbral_Q15
+      estado.umbralesQB = umbral_QB
+      estado.QTR10 = umbral_Q10
+      estado.QTQ = umbral_QTQ
+      if estado.h_umbrales is not None:
+        estado.setear_umbrales([umbral_Q15, umbral_QB, umbral_QTQ, umbral_Q10])
+      return df
     #Pruebas con el archivo
     df = pd.read_csv("est_25027400_limpia.csv")
     x=QAnla(df)
